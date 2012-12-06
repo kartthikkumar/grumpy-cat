2efd4062a013f20f0062f2f5339f7e83cfca3424
Supreet <supreets123@gmail.com>
2012-12-05 23:54:10 -0800

    Added two new activities

--- MemeWorld/src/com/supreet/memeworld/HomeActivity.java
+++ MemeWorld/src/com/supreet/memeworld/HomeActivity.java
@@ -1,21 +1,41 @@
 package com.supreet.memeworld;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class HomeActivity extends Activity {
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.SherlockActivity;
 
+public class HomeActivity extends SherlockActivity {
+
+	public Activity mActivity = this;
+	ActionBar ab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-    }
+        ab = getSupportActionBar();
+        setTitle("HEHE");
+        ab.setHomeButtonEnabled(false);
+        Button b = (Button) findViewById(R.id.list_button);
+        b.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(mActivity, MemeListActivity.class);
+				startActivity(i);
+				
+			}
+		});
+    }     
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(com.actionbarsherlock.view.Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_home, menu);
+        getSupportMenuInflater().inflate(R.menu.activity_home, menu);
         return true;
     }
     
